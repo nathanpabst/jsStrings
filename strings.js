dad857806e46f13406e0ba7d15e21a49b6af08ba
Nathan Pabst <nathan.pabst@gmail.com>
2018-02-26 20:31:25 -0600

    completed exercise 2

--- strings.js
+++ strings.js
@@ -2,3 +2,8 @@
 
 var numOfSandwiches = 2 * 4;
 console.log("numOfSandwiches", numOfSandwiches)
+
+//challenge 2
+var name = "bob";
+var string = "hello, " + name + " how are you doing today?";
+console.log("string:", string);
